448bd473f8ec9569b4f32073fa8daa18666401ae
vamshi7589 <vamshi7589@gmail.com>
2020-11-09 22:45:16 +0530

    added : added monthly wage program

--- EmpWage.java
+++ EmpWage.java
@@ -85,3 +85,36 @@ class EmployeeWageUC4
         System.out.println("Employee Wage :"+empwage);
 	}
 }
+class EmployeeWageUC5
+{
+
+	public static void main(String[] args)
+	{
+		final int isfull=1;
+		final int isparttime=2;
+		int emprateperhour=20;
+		int emphours=0;
+		int empwage=0;
+		int totalworkingdays=20;
+		int monthsalary=0;
+		int i=0;
+		for (i=0;i<totalworkingdays;i++){
+		double empcheck=Math.floor(Math.random() *10)%3;
+		switch((int)empcheck){
+		case isfull:
+			emphours=8;
+			break;
+		case isparttime:
+			emphours=4;
+			break;
+		default:
+			emphours=0;
+		}
+	empwage=emphours*emprateperhour;
+        monthsalary+=empwage;
+	}
+      System.out.println("Monthly Wage :"+monthsalary);
+
+	}
+
+}
