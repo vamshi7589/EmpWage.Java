3235c7c695e5c894dd9ad52da1f5b6ec3d6aff29
vamshi7589 <vamshi7589@gmail.com>
2020-11-09 22:37:01 +0530

    added : added part time employee wage program

--- EmpWage.java
+++ EmpWage.java
@@ -33,3 +33,30 @@ class EmployeeWageUC2
 		}
 	}
 }
+class EmployeeWageUC3
+{
+
+	public static void main(String[] args)
+	{
+		int isfull=1;
+		int isparttime=2;
+		int emprateperhour=20;
+		int emphours=0;
+		int empwage=0;
+		double empcheck=Math.floor(Math.random() *10)%3;
+		if(empcheck==isfull)
+		{
+			emphours=8;
+		}
+		else if(empcheck==isparttime)
+		{
+			emphours=4;
+		}
+		else
+		{
+			emphours=0;
+		}
+	empwage=emphours*emprateperhour;
+        System.out.println("Employee Wage :"+empwage);
+	}
+}
