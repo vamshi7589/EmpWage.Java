c29dea23d4d25bbb3329e990ac58b8dd4f6403fb
vamshi7589 <vamshi7589@gmail.com>
2020-11-09 22:32:09 +0530

    added: added employee wage calculation program

--- EmpWage.java
+++ EmpWage.java
@@ -9,4 +9,27 @@ public class EmployeeWage{
 		else
 			System.out.println("Employee absent");
 	}
-}}
+}
+class EmployeeWageUC2
+{
+
+	public static void main(String[] args)
+	{
+		int isfull=1;
+		int emprateperhour=20;
+		int emphours=0;
+		int empwage=0;
+		double empcheck=Math.floor(Math.random() *10)%2;
+		System.out.print("Employee Wage :");
+		if(empcheck==1)
+		{
+			emphours=8;
+			empwage=emphours*emprateperhour;
+			System.out.println(empwage);
+		}
+		else
+		{
+			System.out.println(empwage);
+		}
+	}
+}
