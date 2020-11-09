8bb1f865b4d176dc47b8db64f01e8dcb50686eac
vamshi7589 <vamshi7589@gmail.com>
2020-11-09 22:48:51 +0530

    added : added program to calculate wage till condition

--- EmpWage.java
+++ EmpWage.java
@@ -118,3 +118,38 @@ class EmployeeWageUC5
 	}
 
 }
+class EmployeeWageUC6
+{
+
+	public static void main(String[] args)
+	{
+		final int isfull=1;
+		final int isparttime=2;
+		int emprateperhour=20;
+		int emphours=0;
+		int empwage=0;
+		int totalworkingdays=20;
+		int monthsalary=0;
+		int hours=0;
+		int maxhours=100;
+		while(hours<maxhours){
+		double empcheck=Math.floor(Math.random() *10)%3;
+		switch((int)empcheck){
+		case isfull:
+			emphours=8;
+			break;
+		case isparttime:
+			emphours=4;
+			break;
+		default:
+			emphours=0;
+		}
+	hours+=emphours;
+	empwage=emphours*emprateperhour;
+        monthsalary+=empwage;
+	}
+      System.out.println("Monthly Wage :"+monthsalary);
+
+	}
+
+}
