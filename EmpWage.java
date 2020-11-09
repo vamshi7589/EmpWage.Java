d60e4cef22d2838c257a557f7abe2930cac526d7
vamshi7589 <vamshi7589@gmail.com>
2020-11-09 22:41:30 +0530

    added: added switch case program

--- EmpWage.java
+++ EmpWage.java
@@ -60,3 +60,28 @@ class EmployeeWageUC3
         System.out.println("Employee Wage :"+empwage);
 	}
 }
+class EmployeeWageUC4
+{
+
+	public static void main(String[] args)
+	{
+		final int isfull=1;
+		final int isparttime=2;
+		int emprateperhour=20;
+		int emphours=0;
+		int empwage=0;
+		double empcheck=Math.floor(Math.random() *10)%3;
+		switch((int)empcheck){
+		case isfull:
+			emphours=8;
+			break;
+		case isparttime:
+			emphours=4;
+			break;
+		default:
+			emphours=0;
+		}
+	empwage=emphours*emprateperhour;
+        System.out.println("Employee Wage :"+empwage);
+	}
+}
